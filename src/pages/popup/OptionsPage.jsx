d3b5846e3350179407a1eec532953b59b7f96a39
Im-Jota <joseluisromeroarce12@gmail.com>
2025-03-27 18:19:03 -0500

    dded user info and logout button

--- src/pages/popup/OptionsPage.jsx
+++ src/pages/popup/OptionsPage.jsx
@@ -1,8 +1,12 @@
 import { Flex, Link } from 'theme-ui'
+import styled from 'styled-components'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import { isFirefox, websiteUrl } from 'utils/constants'
 import PageHeader from './PageHeader'
 import Toggle from './Toggle'
-import SignInComponent from '../../utils/login.ts';
+import Authentification from '../../utils/authentification.ts';
 import Token from '../../utils/token.ts';
 
 const OptionsPage = ({
@@ -13,38 +17,54 @@ const OptionsPage = ({
   setDisableWebRtc,
   messages,
 }) => {
-  const openLink = () => {
-    const signIn = new SignInComponent();
-    signIn.signIn();
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const profile = process.env.PASSPORT_SERVER + '/about';
+
+  useEffect(() => {
+    const validated = new Token();
+
+    const fetchData = async () => {
+      const token = await validated.getDecryptedToken();
+      const isValidated = await validated.verificate();
+      try {
+        // Hacemos una llamada a una API o cualquier operación asíncrona
+        if(isValidated == true) {
+          const data = await axios.get(`${process.env.PASSPORT_SERVER}/api/gateway/user`, {
+              headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded',
+                  Accept: 'application/json',
+                  Authorization: `Bearer ${token}`
+              },
+            });
+          setData(data.data);
+        } else {
+            console.log('token invalited');
+        }
+      } catch (error) {
+        // En caso de error, guardamos el error en el estado
+        setError(error)
+      } finally {
+        // Cambiamos el estado de carga a false una vez que se complete la operación
+        setLoading(false)
+      }
+    }
+    fetchData();
+  }, [])
+  const logout = async () => {
+    const authentification = new Authentification();
+    await authentification.logout();
+    console.log('llll')
   }
 
-  const validated = () => {
-    const token = new Token();
-    token.verificate();
+  if (loading) {
+    return <Loading><span>Loading...</span></Loading>;
   }
 
-  const handleSpoofGeolocationToggle = () => {
-    chrome.storage.local.set({
-      spoofGeolocation: !spoofGeolocation,
-    })
-
-    setSpoofGeolocation(!spoofGeolocation)
-  }
-
-  const handleDisableWebRtcToggle = () => {
-    const disableWebRtcValue = isFirefox
-      ? 'proxy_only'
-      : 'disable_non_proxied_udp'
-
-    chrome.privacy.network.webRTCIPHandlingPolicy.set({
-      value: disableWebRtc ? 'default' : disableWebRtcValue,
-    })
-
-    chrome.storage.local.set({
-      disableWebRtc: !disableWebRtc,
-    })
-
-    setDisableWebRtc(!disableWebRtc)
+  if (error) {
+    return <div>{error}</div>;
   }
 
   return (
@@ -57,60 +77,68 @@ const OptionsPage = ({
         }}
       >
         {!sessionAuthToken && (
-          <Flex
-            sx={{
-              gap: '20px',
-              py: '20px',
-              borderBottom: '1px solid',
-              borderColor: 'darkGrey',
-            }}
-          >
-            <Link
-              id="signupButton"
-              onClick={validated}
-              variant="styles.baseButton"
-            >
-              {messages.signUp}
-            </Link>
-            <Link
-              id="loginButton"
-              variant="styles.baseButton"
-              onClick={openLink}
+          <>
+            <Flex
               sx={{
-                bg: 'transparent',
-                color: 'blue',
+                flexDirection: 'column',
+                alignItems: 'center',
               }}
             >
-              {messages.login}
-            </Link>
-          </Flex>
+              <img src="" />
+              <span>{ data.name } { data.last_name } </span>
+              <span>{ data.email }</span>
+            </Flex>
+            <Flex
+              sx={{
+                gap: '20px',
+                py: '20px',
+                borderBottom: '1px solid',
+                borderColor: 'darkGrey',
+              }}
+            >
+              <Link
+                id="signupButton"
+                variant="styles.baseButton"
+                href={profile}
+                target="_blank"
+              >
+                Edit Profile
+              </Link>
+              <Link
+                id="loginButton"
+                variant="styles.baseButton"
+                onClick={logout}
+                sx={{
+                  bg: 'transparent',
+                  color: 'blue',
+                }}
+              >
+                Logout
+              </Link>
+            </Flex>
+          </>
         )}
-        <Flex
-          sx={{
-            flexDirection: 'column',
-            gap: '20px',
-            pt: '20px',
-          }}
-        >
-          <Toggle
-            title={messages.spoofGeolocation}
-            subTitle={messages.spoofGeolocationSubTitle}
-            id="spoofGeolocationToggle"
-            checked={spoofGeolocation}
-            onToggle={() => handleSpoofGeolocationToggle()}
-            showBorder
-          />
-          <Toggle
-            title={messages.disableWebRtc}
-            subTitle={messages.disableWebRtcSubTitle}
-            id="disableWebRtcToggle"
-            checked={disableWebRtc}
-            onToggle={() => handleDisableWebRtcToggle()}
-          />
-        </Flex>
       </Flex>
     </PageHeader>
   )
 }
+
+const Button = styled.button`
+  width: 100%;
+  border: none;
+  padding: .6rem;
+  border-radius: .4rem;
+`;
+
+const Loading = styled.div`
+  height: 220px;
+  display: flex;
+  justify-content: center;
+align-items: center;
+
+  span {
+    display: block;
+  }
+`;
 
 export default OptionsPage
